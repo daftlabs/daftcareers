714005b55ad118e46712c2578def5ac0b65aa8c8
Ben Stein <mrben522@gmail.com>
2016-04-07 10:41:37 -0400

    making it work?

--- app/scripts/satisfaction.js
+++ app/scripts/satisfaction.js
@@ -1,1 +1,20 @@
-document.getElementById("satisfaction").submit();
+window.addEventListener("DOMContentLoaded", function() {
+    function getQueryVariable(variable)
+    {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+        return(false);
+}
+    }, false); 
+
+document.onload function() {
+    var satisfaction = getQueryVariable("satisfaction");
+    var origin = getQueryVariable("origin");
+    getElementById("result").value = satisfaction;
+    getElementById("origin").value = origin;
+    getElementById("satisfaction").submit();
+}
