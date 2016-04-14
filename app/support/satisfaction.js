8cd52bcaa9d9c8873d2c1c1006333df825a1df7b
Ben Stein <mrben522@gmail.com>
2016-04-14 14:35:35 -0400

    more of that

--- app/support/satisfaction.js
+++ app/support/satisfaction.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
   var satisfaction = getQueryVariable('satisfaction');
   var origin = getQueryVariable('origin');
   document.getElementById('result').value = satisfaction;
