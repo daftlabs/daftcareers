39003409eae263247f6404dcf8e80accb6d05df1
Ben Stein <mrben522@gmail.com>
2016-04-14 13:43:08 -0400

    changing to AJAX request to avoid ugly redirect

--- app/scripts/satisfaction.js
+++ app/scripts/satisfaction.js
@@ -1,12 +1,20 @@
-window.onload = function () {
+jQuery(document).ready(function($) {
   var satisfaction = getQueryVariable('satisfaction');
   var origin = getQueryVariable('origin');
   document.getElementById('result').value = satisfaction;
   document.getElementById('origin').value = origin;
   document.getElementById('ip').value = myip;
-  document.getElementById('satisfaction').submit();
-}
-
+});
+$( window ).load(function() {
+  var formData = $('form').serialize();
+  $.ajax({
+    url: 'https://script.google.com/macros/s/AKfycbxtl0qaUunta4r6QaqSNrQookBQ6SuGN2h8VzPvn8MzK_MDDJkw/exec',
+    data: formData,
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+  });
+});
 function getQueryVariable(variable) {
   var vars = window.location.search.substring(1).split('&');
   for (var i = 0; i < vars.length; i++) {
